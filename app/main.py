25ba863c9fa1eaecd3c01f14e8c9c2da233940ec
rashed <rashedulislam.shajol@gmail.com>
2026-05-07 22:49:47 +0600

    registration

--- app/main.py
+++ app/main.py
@@ -115,7 +115,7 @@ def create_course(course: schemas.CourseCreate, db: Session = Depends(get_db)):
 
 
 @app.post("/users",status_code=status.HTTP_201_CREATED)
-def user(user:schemas.UserCreate,db:Session=Depends(get_db)):
+def users(user:schemas.UserCreate,db:Session=Depends(get_db)):
     new_user = models.User(**user.model_dump())
     db.add(new_user)
     db.commit()
