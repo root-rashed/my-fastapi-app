c3fae118445d1aded5f74ed25650524aa972d23c
rashed <rashedulislam.shajol@gmail.com>
2026-05-24 17:34:39 +0600

    update

--- app/database.py
+++ app/database.py
@@ -11,7 +11,9 @@ SQLALCHEMY_DATABASE_URL = 'postgresql://postgre:125790R@localhost/courses'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
 Base = declarative_base()
+
 
 
 
