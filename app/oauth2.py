eefa1eddd7736a25ef8fbbd936820ae69813ef4e
rashed <rashedulislam.shajol@gmail.com>
2026-06-05 23:19:53 +0600

    update

--- app/oauth2.py
+++ app/oauth2.py
@@ -16,3 +16,4 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter,status,HTTPException,Depends,responses
 from sqlalchemy.orm import Session
-from .. import database,models,utils,schemas
-
+from .. import database,models,utils,schemas,oauth2
+from datetime import time,timedelta,timezone
 
 router = APIRouter(tags=['Authentication'])
 
@@ -15,3 +15,10 @@ def login(user_credentials : schemas.Userlogin,db:Session=Depends(database.get_d
     
     if not utils.verify_password(user_credentials.password,user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid credential")
+    
+    access_token = oauth2.create_access_token(
+        data= {"user_id": user.id},
+        expires_delta= timedelta(minutes=oauth2.ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+
+    return {'access_token':access_token, "token_type":"bearer"}
